af76a4d1235b81bed4d6745b48d3aad979155beb
chanipad <90752351+chanipad@users.noreply.github.com>
2024-09-10 23:22:26 +0200

    json package

--- Day3/server.js
+++ Day3/server.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World! by express')
 })
 
 app.get('/hello/:name', (req, res) => {
-    res.send('hello ' + req.params.name)
+    res.send('hello ' + req.params.name);
 })
 
-app.listen(3001)
+app.get('/hi/:name/:age', (req, res) => {
+    const name = req.params.name;
+    const age = req.params.age;
+    //res.send('name = ' + name + ' age = ' + age);
+    res.send(`name = ${name} age = ${age}`)
+})
+
+app.post('/hello', (req, res) => {
+    res.send(req.body);
+})
+app.put('/myPut', (req, res) => {
+    res.send(req.body);
+})
+
+app.listen(3001);
